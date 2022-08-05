12c7749c811331f14c19965479c8104679776751
sPreet247 <samre2472@gmail.com>
2022-08-04 21:37:40 -0400

    element

--- client/src/App.js
+++ client/src/App.js
@@ -8,8 +8,8 @@ import Join from "./components/Join/Join.js";
 const App = () => {
   <Router>
     <Routes>
-      <Route path="/" exact component={Join} />
-      <Route path="/chat" component={Chat} />
+      <Route path="/" exact element={<Join />} />
+      <Route path="/chat" element={<Chat />} />
     </Routes>
   </Router>;
 };
